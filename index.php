84b1874cf4908667f30caee2b6bfcc202ca532d1
3º ETIM - Desenvolvimento de Sistemas <ds-etim-3@servidor.acad>
2023-05-03 15:21:41 -0300

    Versão 1.0

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pitaya</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
+
+</head>
+<header>
+
+  <nav class="navbar navbar-expand" id="navbar" style="position:fixed">
+    <div class="container-fluid-center" img class="logo" src="imgs/logo.png" alt="logo">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-targ et="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li id="itemnav">
+            <div> <a class="nav-link" href="#produto" style="color: #FF548A;">Produto</a>
+          </li>
+          <li id="itemnav">
+            <a class="nav-link" href="#objetivos" style="color: #FF548A;">Objetivos</a>
+          </li>
+          <li id="itemnav">
+            <a class="nav-link" href="#" style="color: #FF548A;">Equipe</a>
+          </li>
+          <li id="itemnav">
+            <a class="nav-link" href="#" style="color: #FF548A;">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+
+
+
+
+<div class="banner">
+
+<br>
+<br>
+<br>
+
+<div class="container p-5">
+
+<img class="img-home" src="imgs/home.png" alt="home">
+
+  <div class="col-5">
+    <p class="title-desc">O que é PITAYA?</p>
+    <hr>
+    PITAYA é um grupo criado com o intuito de desenvolver
+    tecnologias voltadas para o cotidiano das pessoas.
+
+  </div>
+</div>
+</div>
+</header>
+
+
+<div class="container p-5">
+  <div id="produto">
+    <div class="rol-8">
+      <div><p class="title-prod">Alimentador automático de pets</p></div>
+      Este produto tem a finalidade de facilitar a alimentação dos animais
+      e economizar o tempo dos pais de pets. Através de um aplicativo que
+      você pode baixar no seu celular diretamente da play store, você poderá
+      liberar a ração e água do pet da distância que você estiver, podendo
+      também definir as quantidades necessárias, e tudo isso fora ou dentro
+      do mesmo ambiente que o animal.
+    </div>
+  </div>
+  <img class="comedouro" src="imgs/comedouro-copia.png" alt="comedouro">
+</div>
+
+
+
+
+
+
+
+
+<div class="banner">
+<div class="container">
+<img class="comedouro2" src="imgs/comedouro2-copia.png" alt="comedouro2">
+
+
+<div class="col-5">
+    <p class="produto">Produto</p>
+ <div class="text p-5">O equipamento será construído por meio de canos e mangueiras, os quais terão
+  sensores e outras peças do Arduino. Esse equipamento pode estar em vários
+  ambientes, como: em casa, hotéis de pets, adestramentos e petshops.
+<br><br>
+  Todos os donos que amam os seus pets, sabem da importância de alimentá-los
+  bem. Mas nem sempre conseguem seguir horários regulados e saudáveis, seja
+  por falta de tempo ou viagens. É por isso que a nossa equipe criou o
+  (nome do produto): um equipamento que fornece ração aos animais atráves
+  do celular.
+  </div>
+</div>
+</div>
+
+<div class="text-prod">
+  Tecnologia revolucionária e bem útil. É claro que o ideal é você interagir diretamente com o seu pet
+  e este aparelho não deve substituir um dono. Mas em uma viagem prolongada ou em um dia de trabalho 
+  mais corrido, com alguns simples toques em seu smartphone, você disponibiliza a quantidade certa de 
+  ração na vasilha do pet.
+  <br><br>
+  Aos que têm cachorro, o adestrador Glauco Lima já alerta que os animais que nunca ficaram sozinhos 
+  vão demorar cerca de três dias para se acostumar. “O cachorro que nunca ficou sozinho vai ficar uns 
+  três dias sem se alimentar direito por estar em uma situação nova. Mas aqueles mais acostumados com 
+  a ausência do dono só vão se sentir como se estivessem “mudando da casa da mãe para a casa da avó”.
+  <br><br>
+  Além de tudo, em casos do dia a dia, esse equipamento facilitará a rotina dos donos, estes poderão 
+  economizar tempo, ao invés de alimentar cada pet por vez, poderão alimentá-los com apenas um click 
+  no aplicativo.     
+  </div>
+  </div>
+
+
+  <div><p class="title-prod2">Objetivos do projeto</p></div>
+
+
+ 
+  <div class="row pb-5" id="carro">
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+          aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+          aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+          aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3"
+          aria-label="Slide 4"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4"
+          aria-label="Slide 5"></button>
+      </div>
+      <div class="carousel-inner" style="border-radius:10px">
+        <div class="carousel-item active" style="height: auto;">
+          <img src="imgs/slider/img3.webp" class="d-block w-100 img-fluid h-400" style=" filter: brightness(50%);" alt="...">
+          <h4 class="title mt-3" >Economizar tempo</h4>
+            
+          <div class="carousel-caption d-md-block" id="all2" >
+            
+          </div>
+        </div>
+        <div class="carousel-item" style="height: auto;">
+          <img src="imgs/slider/img3.webp" class="d-block w-100"   style="filter: brightness(50%); " alt="...">
+          <h4 class="title mt-3">Manter horários regulados de alimentação</h4>
+            
+          <div class="carousel-caption d-md-block" id="all2">
+            
+          </div>
+        </div>
+        <div class="carousel-item" style="height: auto;">
+          <img src="imgs/slider/img3.webp" class="d-block w-100" style="filter: brightness(50%);" alt="...">
+          <h4 class="title mt-3">Permitir alimentar o pet a distância</h4>
+            
+          <div class="carousel-caption d-md-block" id="all2">
+            
+          </div>
+        </div>
+        <div class="carousel-item" style="height: auto;">
+          <img src="imgs/slider/img3.webp" class="d-block w-100" style="filter: brightness(50%);" alt="...">
+          <h4 class="title mt-3">Facilitar a alimentação em hotéis para pets</h4>
+            
+          <div class="carousel-caption d-md-block" id="all2">
+            
+          </div>
+        </div>
+        x
+      <br>
+      <button  class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev" color="#000000">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true" ></span>
+        <span class="visually-hidden" >Next</span>
+      </button>
+    </div>
+  </div>
+    </div>
+
+
+
+</div>
+</div>
